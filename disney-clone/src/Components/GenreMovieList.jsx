c0fd34dcd880daf129b5c06e5e15762ede143aef
Mhaigh81 <mhaigh@me.com>
2026-02-15 16:22:55 -0500

    all categories showing

--- disney-clone/src/Components/GenreMovieList.jsx
+++ disney-clone/src/Components/GenreMovieList.jsx
@@ -5,7 +5,7 @@ import MovieList from './MovieList'
 function GenreMovieList() {
   return (
     <div>
-        {GenresList.genre.map((item, index) => index<=4&& (
+        {GenresList.genre.map((item, index) =>  (
             <div className='p-8 px-8 md:px-16'>
                 <h2 className='text-[20px] text-white font-bold'>{item.name}</h2>
                 <MovieList genreId={item.id} index_={index}/>
